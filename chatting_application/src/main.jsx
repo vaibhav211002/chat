5785e4660f4997c565c2670e45ce61d68ff7558c
Vaibhav <vaibhavbhatt9666@gmail.com>
2024-03-05 17:27:50 +0530

    code

--- chatting_application/src/main.jsx
+++ chatting_application/src/main.jsx
@@ -5,7 +5,10 @@ import App from './App.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
 
   <>
-      <App />
+  <React.StrictMode>
+  <App />
+  </React.StrictMode>
+     
 
   </>
   
