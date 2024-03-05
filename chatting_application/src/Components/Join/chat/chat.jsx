ff30cd2ee60f2aefbf52492e291293025c8442f7
Vaibhav <vaibhavbhatt9666@gmail.com>
2024-03-05 17:13:26 +0530

    code

--- chatting_application/src/Components/Join/chat/chat.jsx
+++ chatting_application/src/Components/Join/chat/chat.jsx
@@ -1,112 +1,79 @@
-import React, { useEffect, useState } from 'react'
-import { user } from '../Join/Joined'
+import React, { useEffect, useState } from 'react';
 import socketIO from "socket.io-client";
 import "./chat.css";
-// import sendlogo from '../../../images/sendlogo.jpeg'
 import Messages from '../messages/Messages';
-import ScrollToBottom  from "react-scroll-to-bottom";
+import ScrollToBottom from "react-scroll-to-bottom";
+import { user } from '../Join/Joined';
 import { Link } from 'react-router-dom';
 
-
-
 const ENDPOINT = 'https://chat-server-dusky.vercel.app/';
-var socket ; 
-
-
-
 
 const Chat = () => {
-
     const [id, setid] = useState('');
     const [messages, setMessages] = useState([]);
-    const [endmsg, setendmsg] = useState([]);
 
-    const send = ()=>{
-        const message = document.getElementById('chatInput').value ;
-        document.getElementById('chatInput').value = "";
-        socket.emit('message',{message,id})
-        
-    }
+    useEffect(() => {
+        const socket = socketIO(ENDPOINT, { transports: ['websocket'] });
 
-    
+        socket.on('connect', () => {
+            setid(socket.id);
+            console.log('Connected to server');
+        });
 
-useEffect(()=>{
-    socket = socketIO(ENDPOINT,{transports:['websocket']});
+        socket.emit('joined', { user });
 
-    socket.on('connect',()=>{
-        // alert('connected');
-        setid(socket.id);
-        console.log('we are at the client side');
-    })
+        socket.on('Welcome', (data) => {
+            setMessages(currentMessages => [...currentMessages, data]);
+            console.log(data.user, data.message);
+        });
 
-    socket.emit('joined',{user});
+        socket.on('userjoined', (data) => {
+            setMessages(currentMessages => [...currentMessages, data]);
+            console.log(data.user, data.message);
+        });
 
-    socket.on('Welcome',(data)=>{
-        setMessages(currentMessages => [...currentMessages, data]);
-        console.log(data.user , data.message);
-    })
+        socket.on('leave', (data) => {
+            setMessages(messages => ([...messages, data]));
+            console.log(data.user, data.message);
+        });
 
-    socket.on('userjoined',(data)=>{
-        setMessages(currentMessages => [...currentMessages, data]);
-        console.log(data.user,data.message);
-    })
+        socket.on('sendmessage', (data) => {
+            setMessages(currentMessages => [...currentMessages, data]);
+            console.log(data.user, data.message, data.id);
+        });
 
+        return () => {
+            socket.disconnect();
+        };
+    }, []);
 
-    socket.on('leave',(data)=>{
-    setMessages(messages => ([...messages,data]));
-        console.log(data.user,data.message);
-    })
-    console.log(messages);
+    const send = () => {
+        const message = document.getElementById('chatInput').value.trim();
+        if (message !== '') {
+            socket.emit('message', { message, id });
+            document.getElementById('chatInput').value = "";
+        }
+    };
 
-
-
-    return ()=>{
-
-        socket.emit('disconnect');
-        socket.off();
-
-    }
-},[])
-
-useEffect(() => {
-
-    socket.on('sendmessage',(data)=>{
-        console.log(data);
-        setMessages(currentMessages => [...currentMessages, data]);
-        console.log(data.user,data.message,data.id);
-
-    })
-
-  return () => {
-    socket.off();
-
-  }
-}, [])
-
-
-
-
-  return (
-    <div className='chatPage'>
-        <div className='chatContainer'>
-            <div className='header'>
-                <h2>Chat-ting</h2>
-                <a href='/'><button className='btnclose' type="button">X</button></a>
-                <span className='headname'>Hello , {user}</span>
+    return (
+        <div className='chatPage'>
+            <div className='chatContainer'>
+                <div className='header'>
+                    <h2>Chat-ting</h2>
+                    <a href='/'><button className='btnclose' type="button">X</button></a>
+                    <span className='headname'>Hello, {user}</span>
+                </div>
+                <ScrollToBottom className='chatBox'>
+                    {messages.map((item, i) => <Messages key={i} user={item.id === id ? '' : item.user} message={item.message} cls={item.id === id ? 'right' : 'left'} />)}
+                </ScrollToBottom>
+                <div className='inputBox'>
+                    <input onKeyDown={(event) => event.key === 'Enter' ? send() : null} type='text' id='chatInput' />
+                    <button onClick={send} className='sendBtn'>Send</button>
+                </div>
             </div>
-            <ScrollToBottom  className='chatBox'>
-                {messages.map((item,i)=> <Messages user={item.id===id?'':item.user} message={item.message} cls={item.id===id?'right':'left'} />)}                
-            </ScrollToBottom >
-            <div className='inputBox'>
-                <input onKeyDown={(event)=> event.key === 'Enter' ? send() : null} type='text' id='chatInput' />
-                <button onClick={send} className='sendBtn'>send</button>
-            </div>
-
+            <div className='footer'>This website is in Development Phase. Stay tuned for updates. For any suggestion, email: <a href='mailto:vaibhavbhatt9666@gmail.com'>vaibhavbhatt9666@gmail.com</a></div>
         </div>
-        <div className='footer'>This website is in Development Phase . Stay tuned for updates.For any suggestion Mail: <a href='vaibhavbhatt9666@gmail.com'>vaibhavbhatt9666@gmail.com</a></div>
+    );
+};
 
-    </div>
-  )
-}
-
-export default Chat
+export default Chat;
