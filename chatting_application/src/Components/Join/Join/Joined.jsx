fa9efe8542016532a46e72e76bd74e9d27b62085
Vaibhav <vaibhavbhatt9666@gmail.com>
2024-03-06 18:07:06 +0530

    -link and css-

--- chatting_application/src/Components/Join/Join/Joined.jsx
+++ chatting_application/src/Components/Join/Join/Joined.jsx
@@ -23,16 +23,21 @@ export const Joined = () => {
   console.log(name);
 
   return (
-    <div className='JoinPage'>
+    <div>
+          <div className='JoinPage'>
       <div className='JoinContainer'>
         <img src="https://th.bing.com/th/id/OIP.g_jFE_ijXkNaWUHkjzpY7AHaHa?w=188&h=188&c=7&r=0&o=5&dpr=1.9&pid=1.7" alt="logo" />
         <h1>Chat-ting</h1>
         <input onChange={handlechange} type='text' id='joinInput' placeholder='Enter Your Name'/>
         <Link onClick={(e)=>!name?e.preventDefault():null} to="/chat" ><button onClick={senduser} className='joinbtn'>Login</button></Link>
       </div>
-      <div className='footer'>This website is in Development Phase.Stay tuned for updates.<p>For any suggestion Mail: <a href='vaibhavbhatt9666@gmail.com'>vaibhavbhatt9666@gmail.com</a></p></div>
       
     </div>
+
+            <div className='footer'>This website is in Development Phase.Stay tuned for updates.<p>For any suggestion Mail: <a href='vaibhavbhatt9666@gmail.com'>vaibhavbhatt9666@gmail.com</a></p></div>
+
+    </div>
+
   )
 }
 
