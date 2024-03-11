65f77f76532b04ed517a8e9d6f9f60a1f537edfe
Vaibhav <vaibhavbhatt9666@gmail.com>
2024-03-12 01:46:58 +0530

    updated

--- chatting_application/src/App.jsx
+++ chatting_application/src/App.jsx
@@ -3,13 +3,12 @@ import React from 'react'
 import {BrowserRouter as Router , Route ,Routes} from "react-router-dom";
 import { Joined } from './Components/Join/Join/Joined';
 import Chat from './Components/Join/chat/chat';
-import handler from 'error-logger-client'
+
 
 
 
 function App() {
 
-  handler()
   // socket.on("connect",()=>{
   //   console.log('we are in the client');
 
